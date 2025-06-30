17ce0bc5feb52679d322358517e5bbbc877526b5
Kojo <kabdujo@gmail.com>
2025-06-30 12:55:09 -0400

    updated

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+const userListEL = document.querySelector('.user-list');
+
+
+
+async function manin() {
+
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData = await users.json();
+
+    console.log(usersData);
+
+    userListEL.innerHTML = usersData.map(user => userHTML(user)).join('')
+
+}
+manin();
+function showUserPosts(id) {
+    window.location.href = `http://127.0.015500/user.html`;
+   console.log(id);
+}
+
+
+function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPosts(${user.Id})>
+<div class="user-card__container">
+    <h3>${user.name}</h4>
+    <p><b>Email:</b> ${user.email}</p>
+    <p><b>Phone:</b> ${user.phone}/p>
+    <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+</div>
+</div>`;
+}
